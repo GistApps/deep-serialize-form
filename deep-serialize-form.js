d6adb083abf5ddc451487cdcc0cf85113a86b394
Zac Fair <zac@appsbypix.com>
2021-10-27 13:17:11 -0700

    fixed issue with values not pulling correctly based on index

--- deep-serialize-form.js
+++ deep-serialize-form.js
@@ -25,30 +25,14 @@ function deepSerializeForm(form) {
 
   var coerce_types = { 'true': !0, 'false': !1, 'null': null };
 
-  /**
-   * Get the input value from the formData by key
-   * @return {mixed}
-   */
-  var getValue = function(formData, key) {
+  for(var pair of formData.entries()) {
 
-    var val = formData.get(key);
-
-    val = val && !isNaN(val)              ? +val              // number
-        : val === 'undefined'             ? undefined         // undefined
-        : coerce_types[val] !== undefined ? coerce_types[val] // true, false, null
-        : val;                                                // string
-
-    return val;
-  }
-
-  for (var key of formData.keys()) {
-
-    var val  = getValue(formData, key);
+    var key  = pair[0];
+    var val  = pair[1];;
     var cur  = obj;
     var i    = 0;
     var keys = key.split('][');
     var keys_last = keys.length - 1;
-
 
     if (/\[/.test(keys[0]) && /\]$/.test(keys[keys_last])) {
 
@@ -63,6 +47,10 @@ function deepSerializeForm(form) {
       keys_last = 0;
     }
 
+    val = val && !isNaN(val)              ? +val              // number
+        : val === 'undefined'             ? undefined         // undefined
+        : coerce_types[val] !== undefined ? coerce_types[val] // true, false, null
+        : val;
 
     if ( keys_last ) {
 
